8eda057279fa18ecf790fc657ad23dd4ed4e1e8f
derynm <dery02.nm@gmail.com>
2025-03-08 23:46:44 +0700

    inital project

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>halo</h1>
 </template>
+
+<script lang="ts">
+</script>
